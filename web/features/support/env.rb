c65e84a25e7acbb98e26acd5905a9794585cb4fd
sandra-lourenco <65508236+sandra-lourenco@users.noreply.github.com>
2021-02-16 22:05:22 +0000

    Update env.rb

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -13,6 +13,18 @@ when "chrome"
 when "fire_headless"
   @driver = :selenium_headless
 when "chrome_headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+  version = Capybara::Selenium::Driver.load_selenium
+  options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument('--headless')
+    opts.add_argument('--disable-gpu') if Gem.win_platform?
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.add_argument('--disable-site-isolation-trials')
+  end
+
+  Capybara::Selenium::Driver.new(app, **Hash[:browser => :chrome, options_key => browser_options])
+end
   @driver = :selenium_chrome_headless
 else
   raise "Navegador incorreto, variável @driver está vazia .("
